e584e8bbced2821f19b3886d64be929cfbcbe30f
Luke <lflint07@gmail.com>
2020-09-09 21:27:25 -0400

    css styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,11 +4,11 @@ function Main() {
   return (
     <div className="main">
       <div className="component">
-        <div class="image">
-          <img src="images/drawers.jpg" alt="Green dresser" />
+        <div className="image">
+          <img src="/images/drawers.jpg" alt="Green dresser"></img>
         </div>
-        <div class="content-box">
-          <div class="content">
+        <div className="content-box">
+          <div className="content">
             <h3>
               Shift the overall look and feel by adding these wonderful touches
               to furniture in your home
@@ -19,19 +19,19 @@ function Main() {
               help you make any room feel complete.
             </p>
           </div>
-          <div class="content-info">
-            <div class="user-info">
-              <img class="user-avi" />
+          <div className="content-info">
+            <div className="user-info">
+              <img className="user-avi" />
               <p>Michelle Appleton 28 June 2020</p>
             </div>
-            <div class="user-contact"></div>
-            <div class="share">
-              <button class="share-button"></button>
-              <div class="share-module">
+            <div className="user-contact"></div>
+            <div className="share">
+              <button className="share-button"></button>
+              <div className="share-module">
                 <p>SHARE</p>
-                <div class="facebook"></div>
-                <div class="twitter"></div>
-                <div class="pinterest"></div>
+                <div className="facebook"></div>
+                <div className="twitter"></div>
+                <div className="pinterest"></div>
               </div>
             </div>
           </div>
